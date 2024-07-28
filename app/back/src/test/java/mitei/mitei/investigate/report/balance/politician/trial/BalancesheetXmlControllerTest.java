ead335ec7476c6f0783662e4283eb427f37c4e60
Chiwaki <chiwa2023@ymail.ne.jp>
2024-07-28 11:19:48 +0900

    軽微な修正

--- app/back/src/test/java/mitei/mitei/investigate/report/balance/politician/trial/BalancesheetXmlControllerTest.java
+++ app/back/src/test/java/mitei/mitei/investigate/report/balance/politician/trial/BalancesheetXmlControllerTest.java
@@ -1,15 +1,15 @@
 package mitei.mitei.investigate.report.balance.politician.trial;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -24,16 +24,19 @@ import org.springframework.test.web.servlet.MockMvc;
 @DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
 class BalancesheetXmlControllerTest {
 
-    /** MockMvc. */
+    /** MockMvc */
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     void test()throws Exception {
-        String responsContent = this.mockMvc.perform(get("/convert-balancesheet-xml")).andExpect(status().isOk()).andReturn() // NOPMD
-                .getResponse().getContentAsString(StandardCharsets.UTF_8);
+        //String responsContent = this.mockMvc.perform(get("/convert-balancesheet-xml")).andExpect(status().isOk()).andReturn() // NOPMD
+        //        .getResponse().getContentAsString(StandardCharsets.UTF_8);
+        //System.out.println(responsContent);
         
-        System.out.println(responsContent);
+        assertThat(this.mockMvc.perform(get("/convert-balancesheet-xml")).andExpect(status().isOk()).andReturn() // NOPMD
+                .getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+        
     }
 
 }
