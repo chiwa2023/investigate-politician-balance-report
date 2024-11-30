10cfba189fa89de377069bb043672dd89cf6b0c1
Chiwaki <chiwa2023@ymail.ne.jp>
2024-12-01 07:59:29 +0900

    fix assertThat変換Utility(isTrue,isFalseに対応)

--- app/back/src/test/java/mitei/mitei/investigate/report/balance/politician/util/ConvertAsserThatToIsEqualUtil.java
+++ app/back/src/test/java/mitei/mitei/investigate/report/balance/politician/util/ConvertAsserThatToIsEqualUtil.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * assertThatからassertEqualsに変換するUtility
@@ -16,8 +17,14 @@ public class ConvertAsserThatToIsEqualUtil {
     /** assertThatの開始Key */
     private static final String KEY_OPEN = "assertThat(";
 
-    /** assertThatの終了Key */
-    private static final String KEY_CLOSE = "isEqualTo(";
+    /** assertThatの終了Key(equals) */
+    private static final String KEY_CLOSE_EQUALS = "isEqualTo(";
+
+    /** assertThatの終了Key(true) */
+    private static final String KEY_CLOSE_TRUE = "isTrue(";
+
+    /** assertThatの終了Key(false) */
+    private static final String KEY_CLOSE_FALSE = "isFalse(";
 
     /** 新しいファイルの保存場所 TODO 完全に安定して動くようになったら元ファイルを上書きする */
     private static final String SAVE_FOLDER = "c:/temp";
@@ -64,17 +71,22 @@ public class ConvertAsserThatToIsEqualUtil {
             if (isAssert) {
                 stringBuilder.append(line.trim());
             }
-            if (line.indexOf(KEY_CLOSE) != -1) {
+
+            String key = this.useKey(line);
+
+            if (!Objects.isNull(key)) {
+
                 isAssert = false;
+
                 String allTest = stringBuilder.toString();
 
+                // actualを設定
                 int posActualStart = allTest.indexOf(KEY_OPEN) + KEY_OPEN.length();
                 int posActualEnd = allTest.indexOf(").", posActualStart);
                 String actual = allTest.substring(posActualStart, posActualEnd);
 
-                int posExpectStart = allTest.indexOf(KEY_CLOSE) + KEY_CLOSE.length();
-                int posExpectEnd = allTest.indexOf(");", posExpectStart);
-                String expect = allTest.substring(posExpectStart, posExpectEnd);
+                // expectを設定
+                String expect = this.getExpect(allTest, key);
 
                 StringBuilder builderNew = new StringBuilder("assertEquals("); // NOPMD
                 builderNew.append(expect).append(',').append(actual).append(",\"\");");
@@ -85,6 +97,48 @@ public class ConvertAsserThatToIsEqualUtil {
         }
 
         return list;
+    }
+
+    // actual節が終了しているかどうか確認
+    private String useKey(final String line) {
+
+        if (line.indexOf(KEY_CLOSE_EQUALS) != -1) {
+            return KEY_CLOSE_EQUALS;
+        }
+
+        if (line.indexOf(KEY_CLOSE_TRUE) != -1) {
+            return KEY_CLOSE_TRUE;
+        }
+
+        if (line.indexOf(KEY_CLOSE_FALSE) != -1) {
+            return KEY_CLOSE_FALSE;
+        }
+
+        return null;
+    }
+
+    // expectを取得
+    private String getExpect(final String allTest, final String key) {
+        String expect = "";
+        switch (key) {
+            case KEY_CLOSE_EQUALS:
+                int posExpectStart = allTest.indexOf(KEY_CLOSE_EQUALS) + KEY_CLOSE_EQUALS.length();
+                int posExpectEnd = allTest.indexOf(");", posExpectStart);
+                expect = allTest.substring(posExpectStart, posExpectEnd);
+                break;
+
+            case KEY_CLOSE_TRUE:
+                expect = "true";
+                break;
+
+            case KEY_CLOSE_FALSE:
+                expect = "false";
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + key);
+        }
+        return expect;
     }
 
 }
